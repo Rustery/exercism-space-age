ec85c83a54f68b64f849f325b0bc9edc9c47dad5
Rustam <rustery@gmail.com>
2019-12-30 16:20:04 +0300

    make things simpler

--- space_age.go
+++ space_age.go
@@ -1,32 +1,23 @@
-// Exercism side exercise about Space Age.
+// Package space age is Exercism side exercise.
 package space
 
+// Planet type contains planets name
 type Planet string
+
+var periods = map[Planet]float64{
+	"Earth":   1,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
+
+var earthYearInSecs float64 = 31557600
 
 // Age calculates how old someone would be on any planet.
 func Age(f float64, planet Planet) float64 {
-
-	var earthYearInSecs float64 = 31557600
-
-	switch planet{
-	case "Earth":
-		return f / earthYearInSecs
-	case "Mercury":
-		return f / (earthYearInSecs * 0.2408467)
-	case "Venus":
-		return f / (earthYearInSecs * 0.61519726)
-	case "Mars":
-		return f / (earthYearInSecs * 1.8808158)
-	case "Jupiter":
-		return f / (earthYearInSecs * 11.862615)
-	case "Saturn":
-		return f / (earthYearInSecs * 29.447498)
-	case "Uranus":
-		return f / (earthYearInSecs * 84.016846)
-	case "Neptune":
-		return f / (earthYearInSecs * 164.79132)
-	default:
-		return 0
-	}
-
+	return f / (earthYearInSecs * periods[planet])
 }
